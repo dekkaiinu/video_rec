ffd949f426a8482c3cc247fbb702427ade4914c9
dekkaiinu <youtanoboru1354@gmail.com>
2023-06-05 13:38:03 +0900

    add timelaps

--- video_rec.py
+++ video_rec.py
@@ -1,7 +1,9 @@
 import numpy as np
 import os
 import cv2
+import pyrealsense2 as rs
 from datetime import datetime
+import time
 
 """
 CONFIG
@@ -19,22 +21,26 @@ FILEPATH = "./" + datetime.now().strftime("%Y-%m-%d") + "/"
 
 
 def main():
-    set = input("何セット目ですか？:")
     print("撮影開始")
-    print("tを押して撮影時間を記録")
+    print("tを押して撮影時間を記録,depthの撮影")
     print("qを押して終了")
-    capture(set)
+    capture()
     print("撮影完了")
 
-def capture(set):
+def capture():
     num = 0
-    laps = np.zeros(5, dtype=object)
+    start_time = time.time()
+
+    laps_date = np.zeros(500, dtype=object)
+    laps_time = np.zeros(500, dtype=object)
+
     cap = cv2.VideoCapture(CAMERA_DEVISE+cv2.CAP_DSHOW)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, IMAGE_WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, IMAGE_HEIGHT)
     cap.set(cv2.CAP_PROP_FPS, FPS)
+
     fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")  # 動画のコーデックを指定
-    filename = datetime.now().strftime("%Y-%m-%d_") + "SET" + str(set) +".mp4"
+    filename = datetime.now().strftime("%Y-%m-%d_") +".mp4"
     if not os.path.exists(FILEPATH):
         os.makedirs(FILEPATH)
     out = cv2.VideoWriter(FILEPATH + filename, fourcc, FPS, (IMAGE_WIDTH, IMAGE_HEIGHT))  # 出力ファイル名、コーデック、フレームレート、フレームサイズを指定
@@ -53,28 +59,34 @@ def capture(set):
         key = cv2.waitKey(1) & 0xFF
         # 時間を記録する
         if key == ord('t'):
-            laps = rap(laps, num)
+            laps_date = date_rap(laps_date, num)
+            laps_time = time_rap(start_time, laps_time, num)
             num = num + 1
             print(str(num) + "回目")
 
         # 'q'キーを押して終了する
         if key == ord('q'):
+            laps = np.stack([laps_date, laps_time])
             print(laps)
-            np.save(FILEPATH + datetime.now().strftime("%Y-%m-%d_") + "SET" + str(set) +".npy", laps)
+            np.save(FILEPATH + datetime.now().strftime("%Y-%m-%d_") + ".npy", laps)
             break
 
     cap.release()  # カメラキャプチャを解放
     out.release()  # 動画ファイルを閉じる
     cv2.destroyAllWindows()  # ウィンドウを閉じる
 
-            
 
-def rap(laps, num):
+def date_rap(laps, num):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(now)
     laps[num] = now
     return laps 
 
+def time_rap(start_time, laps, num):
+    now = time.time()
+    now_time = now - start_time
+    laps[num] = now_time
+    return laps
 
 
 if __name__ == "__main__":
