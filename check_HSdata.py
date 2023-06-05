c82d8fc901c8ec1f2534e6832493169b8c9771f1
dekkaiinu <youtanoboru1354@gmail.com>
2023-06-05 20:02:51 +0900

    fix check_HSfata.py

--- check_HSdata.py
+++ check_HSdata.py
@@ -5,19 +5,35 @@ import matplotlib.pyplot as plt
 ""
 LOADPATH = "C:/Users/youta/HsRawData2pickle/img02_Img-d(s35,g20,28.45ms,350-1100).pkl"
 WITEPATH = "./a.png"
+COLORS = ["indigo", "blueviolet", "dodgerblue", "lawngreen", "yellow", "orange", "red", "darkred", "maroon","saddlebrown","black","gray","silver","lightgray","gainsboro"]
+LEGENDS = ["1~10", "11~20", "21~30", "31~40", "41~50", "51~60", "61~70", "71~80", "81~90","91~100","101~110","111~120","121~130","131~140","141~150"]
 
+THRESHOLD = 5 #飽和状態を決定する画素値の閾値(上下の)
+SATURATION_RATIO = 0.1 #飽和認定する画素値の比率
+# 2048 * 1080 = 2,211,840  を256(画素値)で割ると1つのビンあたり8640
 ""
 
 def main():
     array = load_data(LOADPATH)
     array = np.clip(array, 0, 255).astype(np.uint8)
+    count = 0
+    rates = []
     for i in range(151):
         band_array = band_image_array(array, i)
-        print(i + 1 , end=": ")
-        print(saturation_detect(band_array))
-    plt.imshow(band_array)
-    cv.imwrite(WITEPATH, band_array)
-    band_image_hist(band_array)
+        rate, frag = saturation_detect(band_array)
+        if frag == True:
+            count += 1
+            rate = round(rate, 2)
+            rate_pr = str(i + 1) + " :" + str(rate)
+            #print(rate_pr)
+            rates.append(rate_pr)
+    np.savetxt('saturation_pixel_rate.csv', rates ,delimiter=',', fmt='%s')
+    print("飽和バンド率：", end="")
+    print("{:.2f}".format(count / i))
+    plot_hist(array)
+    #指定したバンドのヒストグラムを出力
+    select_plot_hist(band_image_array(array, 10))
+
 
 def load_data(name):
     data = np.load(name, allow_pickle=True)
@@ -27,23 +43,49 @@ def band_image_array(array, band):
     band_array = array[:, :, band]
     return band_array
 
-def band_image_hist(band_array):
-    histgram = cv.calcHist([band_array], [0], None, [256], [0, 256])
-    plt.figure(figsize=(8, 6))
-    plt.plot(histgram.flatten(), color='red', alpha=0.5)
-    plt.show()
-
 def saturation_detect(band_array):
     frag = False
-    threshold = 5
+    threshold = THRESHOLD
     saturated_pixels = np.count_nonzero((band_array <= threshold) | (band_array >= 255 - threshold))
     saturated_ratio = saturated_pixels / band_array.size
-    if saturated_ratio >= 0.1:
+    if saturated_ratio >= SATURATION_RATIO:
         frag = True
-        print(saturated_ratio)
-    return frag
+    return saturated_ratio, frag
 
 
+def select_plot_hist(band_array):
+    histgram = cv.calcHist([band_array], [0], None, [256], [0, 256])
+    plt.figure(figsize=(8, 6))
+    plt.plot(histgram.flatten(), color="gray", alpha=0.5)
+    plt.xlim(-5, 256)
+    plt.ylim(0, 50000)
+    plt.show()
+
+def plot_hist(array):
+    tate, yoko, takasa = np.shape(array)
+    shape = (tate, yoko, 15)
+    hist_array = np.zeros(shape, dtype=np.uint8)
+    for i in range(15):
+        start = i * 10
+        end = start + 9
+        sliced_array = array[:, :, start:end]
+        mean_array = np.mean(sliced_array, axis=2)
+        mean_array = np.round(mean_array).astype(np.uint8)
+        hist_array[:, :, i] = mean_array
+    
+    fig, axes = plt.subplots(3, 5, figsize=(10, 5))
+    for i, ax in enumerate(axes.flatten()):
+        histgram = cv.calcHist([hist_array[:, :, i]], [0], None, [256], [0, 256])
+        ax.plot(histgram, color=COLORS[i], alpha=0.8)
+        ax.set_title(LEGENDS[i])
+        ax.set_xlim(-5, 256)
+        ax.set_ylim(0, 50000)
+    
+    plt.tight_layout()
+    plt.savefig("./hist.png")
+    plt.show()
+
+    
 
 if __name__=="__main__":
     main()
