bb0926f1613847e378ee15cebe69ac64b0076a90
dekkaiinu <youtanoboru1354@gmail.com>
2023-06-15 17:30:03 +0900

    update

--- rec/lider_demo.py
+++ rec/lider_demo.py
@@ -1,6 +1,12 @@
 import pyrealsense2 as rs
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
+import os
+
+UPPATH = "./depth/"
+if not os.path.exists(UPPATH):
+    os.makedirs(UPPATH)
 
 # RealSenseパイプラインの初期化
 pipeline = rs.pipeline()
@@ -10,28 +16,44 @@ config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30)
 # パイプラインを開始
 pipeline.start(config)
 
+i = 0
 try:
-    # フレームを待機
-    frames = pipeline.wait_for_frames()
+    while True:
+        # フレームを待機
+        frames = pipeline.wait_for_frames()
 
-    # 深度フレームを取得
-    depth_frame = frames.get_depth_frame()
+        # 深度フレームを取得
+        depth_frame = frames.get_depth_frame()
 
-    # フレームの幅と高さを取得
-    width = depth_frame.get_width()
-    height = depth_frame.get_height()
+        # フレームの幅と高さを取得
+        width = depth_frame.get_width()
+        height = depth_frame.get_height()
 
-    # フレームをNumPy配列に変換
-    depth_image = np.asanyarray(depth_frame.get_data())
+        # フレームをNumPy配列に変換
+        depth_image = np.asanyarray(depth_frame.get_data())
+        depth_image_normalized = (depth_image / 65535 * 255).astype(np.uint8)
 
-    # 深度情報の表示（例: 画素 (100, 200) の深度値）
-    depth_value = depth_image[200, 100]
-    print("Depth value at pixel (100, 200):", depth_value)
+        heatmap = cv2.applyColorMap(depth_image_normalized, cv2.COLORMAP_JET)
+        # 深度画像を表示
+        cv2.imshow("Depth Image", heatmap)
+        # キーボードの入力を監視
+        key = cv2.waitKey(1)
+
+        # "t"キーを押した場合、深度画像を保存
+        if key == ord('t'):
+            i+=1
+            np.save(UPPATH + str(i) + "depth.npy", depth_image)
+            print("Depth " + str(i) + "saved.")
+
+        # "q"キーを押した場合、パイプラインを停止してループを終了
+        if key == ord('q'):
+            break
+
 
 finally:
     # パイプラインを停止してリソースを解放
     pipeline.stop()
 
-plt.imshow(depth_image, cmap="gray")
-plt.colorbar()
-plt.show()
+# ウィンドウを閉じる
+cv2.destroyAllWindows()
+
--- rec/video_rec.py
+++ rec/video_rec.py
@@ -8,14 +8,13 @@ import math
 import sys
 
 ""
-CAMERA_DEVISE = int(sys.argv[1])
 
-IMAGE_WIDTH = 1280
-IMAGE_HEIGHT = 720
-FPS = 15.0
+IMAGE_WIDTH = 1024
+IMAGE_HEIGHT = 768
+FPS = 30
 
-FILEPATH = "./ueno_zoo/penguin/" + datetime.now().strftime("%Y-%m-%d") + "/set" + sys.argv[2] + "/"
-#FILEPATH = "D:/ueno_zoo/penguin/" + datetime.now().strftime("%Y-%m-%d") + "/set" + sys.argv[2] + "/"
+#FILEPATH = "./ueno_zoo/penguin/" + datetime.now().strftime("%Y-%m-%d") + "/set" + sys.argv[1] + "/"
+FILEPATH = "D:/ueno_zoo/penguin/" + datetime.now().strftime("%Y-%m-%d") + "/set" + sys.argv[1] + "/"
 DEPTHPATH = FILEPATH + "depth/"
 
 ""
@@ -29,61 +28,61 @@ def main():
     print("撮影完了")
 
 def capture():
-    num = 0
-    start_time = time.time()
-
     laps_date = np.zeros(500, dtype=object)
     laps_time = np.zeros(500, dtype=object)
 
-    cap = cv2.VideoCapture(CAMERA_DEVISE+cv2.CAP_DSHOW)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, IMAGE_WIDTH)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, IMAGE_HEIGHT)
-    cap.set(cv2.CAP_PROP_FPS, FPS)
-
-    fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")  # 動画のコーデックを指定
-    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") +".mp4"
     if not os.path.exists(FILEPATH):
         os.makedirs(FILEPATH)
     if not os.path.exists(DEPTHPATH):
         os.makedirs(DEPTHPATH)
-    out = cv2.VideoWriter(FILEPATH + filename, fourcc, FPS, (IMAGE_WIDTH, IMAGE_HEIGHT))  # 出力ファイル名、コーデック、フレームレート、フレームサイズを指定
-
-    # RealSenseパイプラインの設定
+    # パイプラインを開始 RealSenseパイプラインの初期化
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-    
+    config.enable_stream(rs.stream.depth, IMAGE_WIDTH, IMAGE_HEIGHT, rs.format.z16, FPS)
+    pipeline.start(config)
 
-    while cap.isOpened():
-        ret, frame = cap.read()  # フレームを取得
+    i = 0
+    start_time = time.time()
+    try:
+        while True:
+            
+            # フレームを待機
+            frames = pipeline.wait_for_frames()
 
-        if not ret:
-            break
+            # 深度フレームを取得
+            depth_frame = frames.get_depth_frame()
 
-        # フレームを表示する
-        cv2.imshow('Frame', frame)
-        # フレームを動画ファイルに書き込む
-        out.write(frame)
+            # フレームをNumPy配列に変換
+            depth_image = np.asanyarray(depth_frame.get_data())
+            depth_image_normalized = (depth_image / 65535 * 255).astype(np.uint8)
 
-        key = cv2.waitKey(1) & 0xFF
-        # 時間を記録する
-        if key == ord('t'):
-            laps_date = date_rap(laps_date, num)
-            laps_time = time_rap(start_time, laps_time, num)
-            depth = capture_depth(pipeline, config)
-            np.save(DEPTHPATH + "DepthImage_"+ datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".npy", depth)
-            num = num + 1
-            print(str(num) + "回目")
+            heatmap = cv2.applyColorMap(depth_image_normalized, cv2.COLORMAP_JET)
+            # 深度画像を表示
+            cv2.imshow("Depth Image", heatmap)
+            # キーボードの入力を監視
+            key = cv2.waitKey(1)
 
-        # 'q'キーを押して終了する
-        if key == ord('q'):
-            laps = np.stack([laps_date, laps_time])
-            np.save(FILEPATH + "time_" +datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".npy", laps)
-            break
+            # "t"キーを押した場合、深度画像を保存
+            if key == ord('t'):
+                laps_date = date_rap(laps_date, i)
+                laps_time = time_rap(start_time, laps_time, i)
+                i+=1
+                np.save(DEPTHPATH + "DepthImage_"+ datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".npy", depth_image)
+                print("Depth " + str(i) + "saved.")
 
-    cap.release()  # カメラキャプチャを解放
-    out.release()  # 動画ファイルを閉じる
-    cv2.destroyAllWindows()  # ウィンドウを閉じる
+            # "q"キーを押した場合、パイプラインを停止してループを終了
+            if key == ord('q'):
+                laps = np.stack([laps_date, laps_time])
+                np.save(FILEPATH + "time_" +datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".npy", laps)
+                break
+
+
+    finally:
+        # パイプラインを停止してリソースを解放
+        pipeline.stop()
+
+    # ウィンドウを閉じる
+    cv2.destroyAllWindows()
 
 
 def date_rap(laps, num):
@@ -99,24 +98,6 @@ def time_rap(start_time, laps, num):
     print(now_time)
     laps[num] = now_time
     return laps
-
-def capture_depth(pipeline, config):
-    # パイプラインを開始
-    pipeline.start(config)
-    try:
-        # フレームを待機
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-
-        # DepthデータをNumPy配列に変換
-        depth_image = np.asanyarray(depth_frame.get_data())
-
-    finally:
-        # パイプラインを停止
-        pipeline.stop()
-
-    return depth_image
-
 
 if __name__ == "__main__":
     main()
